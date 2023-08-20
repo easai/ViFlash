386f6d3fe29f352eeeee9ffb56ef9ffeb908d166
easai <aksarih@gmail.com>
2023-08-19 20:56:32 -0400

    Remove connect-by-name

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,59 +7,57 @@
 #include <QMessageBox>
 #include <QNetworkReply>
 
-MainWindow::MainWindow(QWidget *parent): QMainWindow(parent)
-    , ui(new Ui::ViFlash)
-    , net_manager(new QNetworkAccessManager(this))
-    , net_reply(nullptr)
-    , m_data_buffer(new QByteArray)
-{
-    ui->setupUi(this);
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent), ui(new Ui::ViFlash),
+      net_manager(new QNetworkAccessManager(this)), net_reply(nullptr),
+      m_data_buffer(new QByteArray) {
+  ui->setupUi(this);
+  connect(ui->pushButton, &QPushButton::clicked, this, &MainWindow::refresh);
+  connect(ui->action_Quit, &QAction::triggered, this, &QApplication::quit);
+  connect(ui->action_About, &QAction::triggered, this, &MainWindow::about);
+  connect(ui->actionAbout_Qt, &QAction::triggered, this,
+          &QApplication::aboutQt);
 }
 
 MainWindow::~MainWindow() {
-    delete ui;
-    delete m_data_buffer;
+  delete ui;
+  delete m_data_buffer;
 }
 
 void MainWindow::dataReadyToRead() {
-    qDebug() << "";
-    m_data_buffer->append(net_reply->readAll());
+  m_data_buffer->append(net_reply->readAll());
 }
 
 void MainWindow::dataReadFinished() {
-    if (net_reply->error()) {
-        qDebug() << "Error : " << QString(*m_data_buffer);
-    } else {
-        QJsonDocument doc = QJsonDocument::fromJson(*m_data_buffer);
-        QJsonArray array = doc.array();
-        for (int i = 0; i < array.size(); i++) {
-            QJsonObject object = array.at(i).toObject();
-            QVariantMap map = object.toVariantMap();
-            QString vi = map["vi"].toString();
-            ui->label_vi->setText(vi);
-            QString en = map["en"].toString();
-            ui->label_en->setText(en);
-        }
-        m_data_buffer->clear();
+  if (net_reply->error()) {
+    qDebug() << "Error : " << QString(*m_data_buffer);
+  } else {
+    QJsonDocument doc = QJsonDocument::fromJson(*m_data_buffer);
+    QJsonArray array = doc.array();
+    for (int i = 0; i < array.size(); i++) {
+      QJsonObject object = array.at(i).toObject();
+      QVariantMap map = object.toVariantMap();
+      QString vi = map["vi"].toString();
+      ui->label_vi->setText(vi);
+      QString en = map["en"].toString();
+      ui->label_en->setText(en);
     }
+    m_data_buffer->clear();
+  }
 }
 
-void MainWindow::on_pushButton_clicked() {
-    const QUrl API_ENDPOINT("https://botudien.pythonanywhere.com/api/nrand/1");
-    QNetworkRequest request;
-    request.setUrl(API_ENDPOINT);
+void MainWindow::refresh() {
+  const QUrl API_ENDPOINT("https://botudien.pythonanywhere.com/api/nrand/1");
+  QNetworkRequest request;
+  request.setUrl(API_ENDPOINT);
 
-    net_reply = net_manager->get(request);
-    connect(net_reply, &QIODevice::readyRead, this, &MainWindow::dataReadyToRead);
-    connect(net_reply, &QNetworkReply::finished, this,
-            &MainWindow::dataReadFinished);
+  net_reply = net_manager->get(request);
+  connect(net_reply, &QIODevice::readyRead, this, &MainWindow::dataReadyToRead);
+  connect(net_reply, &QNetworkReply::finished, this,
+          &MainWindow::dataReadFinished);
 }
 
-void MainWindow::on_action_Quit_triggered() { QApplication::quit(); }
-
-void MainWindow::on_actionAbout_Qt_triggered() { QApplication::aboutQt(); }
-
-void MainWindow::on_action_About_triggered() {
-    QMessageBox::about(this, "About ViFlash",
-                       "ViFlash v1.0\n\nCopyright 2023 easai");
+void MainWindow::about() {
+  QMessageBox::about(this, "About ViFlash",
+                     "ViFlash v1.0\n\nCopyright 2023 easai");
 }
--- mainwindow.h
+++ mainwindow.h
@@ -4,36 +4,31 @@
 #include <QMainWindow>
 #include <QNetworkAccessManager>
 
-
 QT_BEGIN_NAMESPACE
-namespace Ui { class ViFlash; }
+namespace Ui {
+class ViFlash;
+}
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow : public QMainWindow {
+  Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+  MainWindow(QWidget *parent = nullptr);
+  ~MainWindow();
 
 public slots:
-    void dataReadyToRead();
-    void dataReadFinished();
+  void dataReadyToRead();
+  void dataReadFinished();
 
 private slots:
-    void on_pushButton_clicked();
-
-    void on_action_Quit_triggered();
-
-    void on_actionAbout_Qt_triggered();
-
-    void on_action_About_triggered();
+  void refresh();
+  void about();
 
 private:
-    Ui::ViFlash *ui;
-    QNetworkAccessManager *net_manager;
-    QNetworkReply *net_reply;
-    QByteArray *m_data_buffer;
+  Ui::ViFlash *ui;
+  QNetworkAccessManager *net_manager;
+  QNetworkReply *net_reply;
+  QByteArray *m_data_buffer;
 };
 #endif // MAINWINDOW_H
