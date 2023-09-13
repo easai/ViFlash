50cfd18eb8adecd73015c3a5fd595abfc27e8dd3
easai <aksarih@gmail.com>
2023-09-13 13:18:11 -0400

    Replace List with Table.

--- wordlistdialog.cpp
+++ wordlistdialog.cpp
@@ -6,11 +6,22 @@ WordListDialog::WordListDialog(QWidget *parent, const QList<Word> &lst, const QS
     ui(new Ui::WordListDialog)
 {
     ui->setupUi(this);
-    for (int i=0;i<lst.count();i++){
+    int nRows=lst.count();
+    QStringList header;
+    header << target << desc;
+
+    ui->tableWidget->setColumnCount(header.count());
+        ui->tableWidget->setHorizontalHeaderLabels(header);
+
+    for (int i=0;i<nRows;i++){
+        ui->tableWidget->insertRow(i);
         Word word=lst.at(i);
         QString targetWord=word.getValue(target);
+        QTableWidgetItem *targetItem=new QTableWidgetItem(targetWord);
+        ui->tableWidget->setItem(i, 0, targetItem);
         QString descWord=word.getValue(desc);
-        ui->listWidget->addItem(targetWord+" : "+descWord);
+        QTableWidgetItem *descItem=new QTableWidgetItem(descWord);
+        ui->tableWidget->setItem(i, 1, descItem);
     }
 }
 
